5a441064148c9fc0dc5aea64cb2093553be895b1
ownificate <57773251+ownificate@users.noreply.github.com>
2020-02-10 10:03:59 -0600

    Bonus code done

---
+++ Assets/Scripts/Reposition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    float speed = 0.02f;
+    void Start()
+    {
+        transform.position = new Vector3(0,0,0);
+        Debug.Log(transform.position);
+    }
+
+    void Update()
+    {
+        float x = transform.position.x;
+        x += speed;
+        //Upon this line running, the character will fly to the left.
+        transform.position = new Vector3(x,0,0);
+    }
+}
