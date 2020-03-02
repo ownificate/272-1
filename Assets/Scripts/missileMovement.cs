121d64f78d7fdc23769684a0a6b9cbd953ae0204
ownificate <spartun89@yahoo.com>
2020-03-02 13:34:08 -0600

    hw5 work

---
+++ Assets/Scripts/missileMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class missileMovement : MonoBehaviour
+{
+    float x,y;
+    GameObject shipFiring;
+    float speed = 0.04f;
+    void Start()
+    {
+        x = shipFiring.transform.position.x - 1.5f;
+        y = shipFiring.transform.position.y;
+        shipFiring = GameObject.Find("shoooter");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x -= speed;
+        y = transform.position.y;
+
+        transform.position = new Vector3(x,y,0);
+    }
+}
---
+++ Assets/Scripts/rocket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour
+{
+    public GameObject shipFiring;
+    public GameObject missile;
+    public KeyCode fire;
+    float x,y;
+    float speed = 0.04f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(fire))
+        {
+            GameObject tmpMissile;
+            tmpMissile = Instantiate(missile,new Vector3(shipFiring.transform.position.x - 1,shipFiring.transform.position.y - 1,0),Quaternion.Euler(0,0,180)) as GameObject;
+            x -= speed;
+            y = transform.position.y;
+
+            transform.position = new Vector3(x,y,0);
+            Destroy(tmpMissile, 5.0f);
+        }
+    }
+}
